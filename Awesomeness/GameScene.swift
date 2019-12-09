77a9a06fdce719262810bcf42047d7f95b887824
Juliana Smith <clarinetplayer2020@gmail.com>
2019-12-09 10:08:18 -0600

    Animated Duolingo Bird Enemy :)

--- Awesomeness/GameScene.swift
+++ Awesomeness/GameScene.swift
@@ -10,6 +10,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    private var bird = SKSpriteNode()
+    private var birdFlyFrames: [SKTexture] = []
+    
     override func didMove(to view: SKView) {
         run(SKAction.repeatForever(
           SKAction.sequence([
@@ -29,23 +32,37 @@ class GameScene: SKScene {
     }
     
     func addEnemy(){
-        let monster = SKSpriteNode (imageNamed: "enemy")
+        
+        let duolingoAtlas = SKTextureAtlas(named: "EvilDuolingoBird")
+        var flyingFrames: [SKTexture] = []
+        
+        let numImages = duolingoAtlas.textureNames.count
+        for i in 0...numImages-1 {
+            let birdName = "sprite\(i)"
+        flyingFrames.append(duolingoAtlas.textureNamed(birdName))
+        }
+        birdFlyFrames = flyingFrames
+        
+        let firstFrameTexture = birdFlyFrames[0]
+        bird = SKSpriteNode(texture: firstFrameTexture)
         
         //Pick a direction and SPAWN
         let spawnPos = Int.random(in: 1..<5)
         if spawnPos == 1 {
-            monster.position = CGPoint(x: 0 - monster.size.width, y: size.height/16)
+            bird.position = CGPoint(x: 0 - bird.size.width, y: size.height/16)
         } else if spawnPos == 2 {
-            monster.position = CGPoint(x: size.width/16, y: size.height + monster.size.width)
+            bird.position = CGPoint(x: size.width/16, y: size.height + bird.size.width)
         } else if spawnPos == 3 {
-            monster.position = CGPoint(x: size.width + monster.size.width, y: size.height/16)
+            bird.position = CGPoint(x: size.width + bird.size.width, y: size.height/16)
         } else if spawnPos == 4 {
-            monster.position = CGPoint(x: size.width/16, y: -size.height - monster.size.width)
+            bird.position = CGPoint(x: size.width/16, y: -size.height - bird.size.width)
         }
         
-        addChild(monster)
+        addChild(bird)
         
         let actualDuration = Double.random(in: 2.0..<4.0)
+        
+        bird.run(SKAction.repeatForever(SKAction.animate(with: birdFlyFrames, timePerFrame: 0.1, resize: false, restore: true)), withKey: "fly")
         
         let move = SKAction.move(to: CGPoint(x: size.width/16, y: size.height/16), duration: TimeInterval(actualDuration))
         let moveFinished = SKAction.removeFromParent()
@@ -53,7 +70,7 @@ class GameScene: SKScene {
           guard let `self` = self else { return }
           let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
         }
-        monster.run(SKAction.sequence([move, loseAction, moveFinished]))
+        bird.run(SKAction.sequence([move, loseAction, moveFinished]))
         
     }
     
