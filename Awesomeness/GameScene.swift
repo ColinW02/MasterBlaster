949b084d8a8e52a9d67354c5e3163ce2fc40c0e2
Juliana Smith <clarinetplayer2020@gmail.com>
2019-12-13 09:21:17 -0600

    background possible fix?

--- Awesomeness/GameScene.swift
+++ Awesomeness/GameScene.swift
@@ -10,5 +10,9 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background1.jpg")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(background)
+    }
 }
